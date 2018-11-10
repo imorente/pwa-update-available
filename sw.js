dc5ace442ed2ab24e755889230cf5acd5b739140
Irene Morente <imorente@gmail.com>
2018-11-10 13:41:55 -0800

    Fix broken cache response

--- sw.js
+++ sw.js
@@ -1,42 +1,47 @@
-var CACHE_NAME = 'my-site-cache-v1';
-console.log("Hello");
+var CACHE_NAME = "my-site-cache-v2";
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   // Perform install steps
-  console.log("I am installed, prepare for trouble!")
+  console.log("I am installed, prepare for trouble!");
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
-  console.log("New service worker version activated, prepare for even more trouble")
-  self.skipWaiting();
-})
+self.addEventListener("activate", function(event) {
+  console.log(
+    "New service worker version activated, prepare for even more trouble"
+  );
+});
 
-self.addEventListener('fetch', function(event) {
-  console.log("Service worker is handling fetch, prepare for lots of trouble!!")
-  console.log(event.request);
-  if (event.request.url.match(/\/app\.\d+\.js/)) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        cache.match(event.request)
-          .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-              return response;
-            }
+self.addEventListener("fetch", function(event) {
+  console.log(
+    "Service worker is handling fetch, prepare for lots of trouble!!",
+    event.request
+  );
 
-            return fetch(event.request).then(function(networkResponse) {
-              console.log(networkResponse);
-              if (networkResponse.status === 200) {
-                cache.put(event.request, networkResponse.clone());
-              } else if (networkResponse.status === 404) {
-                console.log("You need to refresh your stuff buddy!")
-              }
-
-              return networkResponse;
-            });
-          }
-        )
-      })
-    );
+  if (!event.request.url.match(/\/app\.\d+\.js/)) {
+    return;
   }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(event.request).then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+          console.log(response);
+          switch (response.status) {
+            case 200:
+              cache.put(event.request, response.clone());
+            case 400:
+              console.log("You need to refresh your stuff buddy!");
+          }
+
+          return response;
+        });
+      })
+    )
+  );
 });
