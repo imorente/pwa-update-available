6cdce39f8cc870dbca3d76a8d972fd88d687898a
Irene Morente <imorente@gmail.com>
2018-11-10 13:17:42 -0800

    Activate hook in service worker

--- sw.js
+++ sw.js
@@ -5,3 +5,11 @@ self.addEventListener('install', function(event) {
   // Perform install steps
   console.log("I am installed, prepare for trouble!")
 });
+
+self.addEventListener('activate', function(event) {
+  console.log("New service worker version activated, prepare for even more trouble")
+})
+
+// self.addEventListener('fetch', function(event) {
+//
+// })
