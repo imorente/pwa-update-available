fb20996b08d5f3b1d6d2d337d06e39f4bf8fb9ad
Irene Morente <imorente@gmail.com>
2018-11-10 13:14:21 -0800

    Back to basics

---
+++ sw.js
@@ -0,0 +1,7 @@
+var CACHE_NAME = 'my-site-cache-v1';
+console.log("Hello");
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  console.log("I am installed, prepare for trouble!")
+});
