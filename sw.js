a8eefb40342672effd1e4fdf7954afab23360874
Irene Morente <imorente@gmail.com>
2018-11-10 17:07:19 -0800

    More console logs

--- sw.js
+++ sw.js
@@ -19,24 +19,39 @@ self.addEventListener("fetch", function(event) {
   );
 
   if (!event.request.url.match(/\/app\.\d+\.js/)) {
+    console.log("I'm leaving this request alone");
     return;
   }
+
+  console.log("I'm doing stuff. Watch out!");
 
   event.respondWith(
     caches.open(CACHE_NAME).then(cache =>
       cache.match(event.request).then(response => {
         // Cache hit - return response
         if (response) {
+          console.log("Cache hit! Returning cached response", response);
           return response;
         }
 
+        console.log("Cache miss – fetching", response);
         return fetch(event.request).then(response => {
-          console.log(response);
           switch (response.status) {
             case 200:
+              console.log("Caching this one", response);
               cache.put(event.request, response.clone());
-            case 400:
-              console.log("You need to refresh your stuff buddy!");
+              break;
+            case 404:
+              console.log(
+                "Not found – you need to refresh your stuff buddy!",
+                response
+              );
+              break;
+            default:
+              console.log(
+                "I don't know what to do with this ¯\\_(ツ)_/¯",
+                response
+              );
           }
 
           return response;
